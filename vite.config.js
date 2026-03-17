70e8718880c095771e52b8849ae88a6c5c8cd43b
define-tune <164121287+define-tune@users.noreply.github.com>
2026-03-17 14:48:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/AI-/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
